093ee246d4d83c57d42b0999fdf29a1f82649c14
crchong1 <chong.connor@gmail.com>
2025-12-14 18:12:03 -0500

    updating

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -14,6 +14,19 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=DM+Sans:wght@400;500;700&display=swap"
         />
+        
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-391118279"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-391118279');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
